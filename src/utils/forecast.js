5d45e247a09f5972cb46201db3a390b624e981c4
Michal <Marta@MK-NB-009.OP.local>
2021-04-08 19:48:15 +0200

    New forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -13,7 +13,9 @@ const forecast = ((longitude, latitude, callback) => {
             const temperature = body.current.temperature
             const feelslike = body.current.feelslike
             const weather_descriptions = body.current.weather_descriptions[0]
-            callback(undefined, weather_descriptions + ". It's currently " + temperature + " degress out. It feels like " + feelslike + " degress out.")
+            const pressure = body.current.pressure
+            const humidity = body.current.humidity
+            callback(undefined, weather_descriptions + ". It's currently " + temperature + " degress out. It feels like " + feelslike + " degress out. Pressure is equal to " + pressure + " and humidity is equal to: " + humidity)
             
         }
     })
